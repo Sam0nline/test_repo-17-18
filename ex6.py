724581566aacc7527c5c51d62f1d535aa75012d7
Sam0nline <gocsamo@gmail.com>
2017-10-12 00:08:28 +0100

    updated file with comments for better readability

--- ex6.py
+++ ex6.py
@@ -1,5 +1,6 @@
 your_name = input("What's your name? ")
 print("Nice to meet you " + your_name + "!")
+#requires user input via the prompt in quote marks and returns a response to that input
 
 your_age = int(input("How old are you? "))
 if(your_age < 21):
@@ -8,14 +9,17 @@ elif(your_age > 21):
     print(str(your_age) + "? You are older than me.")
 else:
     print(str(your_age) + "? Same age as me!")
+#checks the user input against a condition that verifies if the value is less/greater than or equal to 21 and present a unique response to each.
 
 your_height = int(input("How tall are you in centimetres? ")) #cm
-if(your_height >= 190):
+if(your_height >= 200):
     print(str(your_height) + " cm is rather tall.")
-elif(your_height <= 130):
+elif(your_height <= 140):
     print(str(your_height) + " cm seems quite short.")
 else:
     print(str(your_height) + " cm is about average.")
+#similarly uses a triple condition verification however it allows for certain values to be included in the lesser/greater than thresholds.
+
 your_weight = int(input("How much do you weigh in kilos? ")) #kg
 if(your_weight / ((your_height / 100)*(your_height / 100)) > 25):
     print("You're " + str(your_weight) + " kilos? You are rather overweight for your size.")
@@ -23,12 +27,17 @@ elif(your_weight / ((your_height / 100)*(your_height / 100)) < 18.5):
     print("You're " + str(your_weight) + " kilos? You are quite underweight for your size.")
 else:
     print("You're " + str(your_weight) + " kilos? That's a healthy weight for your size.")
+#another triple condition check but this time it will calculate BMI as opposed to user's inputted weight and return with responses when calculated.
+
 your_eyes = input("What colour are your eyes? ")
 print(your_eyes + " is a nice colour!")
+#a repeat of the your_name function, modified for eye colour
+
 your_hair = input("What's the colour of your hair? ")
 print(your_hair + " is a nice shade of colour.")
+#a repeat of the your_hair function, modified for hair colour
+
 print("")
 print("Thank you for your time!")
 print("")
-
-#endl
+#a brief message of gratitude to the user for completing the inputs successfully with deliberate line breaks
