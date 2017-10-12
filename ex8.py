13415e7d8a969f2577bf74fc43bd57b3f6018e36
SamOnline <gocsamo@gmail.com>
2017-10-12 10:58:51 +0100

    updated task 1 of week 3 worksheet, functional but untidy

--- ex8.py
+++ ex8.py
@@ -1,12 +1,11 @@
+import math
+
 base_annual_salary = int(input("Enter your initial annual salary: "))
 #input line 1 for the user, written as an integer
 
 semi_annual_raise = float(input("Enter the percent of your semi annual raise, as a decimal: "))
 
-salary_increase = base_annual_salary * semi_annual_raise
-salary_raised = base_annual_salary + salary_increase
-
-monthly_salary = salary_raised / 12
+monthly_salary = base_annual_salary / 12
 #calculates the monthly salary based on the annual salary entered by the user
 
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: ")) #decimal form i.e. 10% = 0.1
@@ -31,12 +30,16 @@ while (current_savings < portion_deposit):
     current_savings = current_savings + ((current_savings * r) + (monthly_salary * portion_saved))
     month_counter = month_counter + 1
     if(month_counter % 6 == 0):
-        salary_raised = salary_raised + salary_increase
-        print("Salary increased as follows: " + str(salary_raised))
-        print("Monthly salary print test after raises: " + str(monthly_salary))
+        base_annual_salary += base_annual_salary * semi_annual_raise
+        monthly_salary = base_annual_salary / 12
+#        print("Salary increased as follows: " + str(base_annual_salary))
+#        print("Monthly salary print test after raises: " + str(monthly_salary))
 #calculates interest and monthly saving for a deposit and increment the month counter if the 'current_savings < portion_deposit' condition is met
 
 print("Number of months: " + str(month_counter))
 #prints the total amount of months required to save, using the inputted variables
 
 #template values: base salary = 80k, salary raise = 5%, monthly save = 10%, dream home = 800k
+
+#salary_increase = base_annual_salary * semi_annual_raise
+#salary_raised = base_annual_salary + salary_increase
