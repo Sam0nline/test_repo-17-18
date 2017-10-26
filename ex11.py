f702a1be45a32e2f032349c629b750d96bf633bf
SamOnline <gocsamo@gmail.com>
2017-10-26 11:35:49 +0100

    updated task 2 of week 4 worksheet, minus the use of 'def function()'

--- ex11.py
+++ ex11.py
@@ -1,33 +1,30 @@
 import random
 from random import randint
 
-guess_count = 1
-#initialises the guess count to 1 and increment for every wrong input attempt
+guess_count = 0
+#initialises the guess count to 0 and increment for every wrong input attempt
 
-my_random = random.randint(1,5)
+random_guess = int
+#states the guess input as type int, overriding any type
+
+my_random = random.randint(1,100)
 #a random int is selected between 1 and 5, inclusive
 
-random_guess = int(input("Guess my random number: "))
-#a variable to require an input for each guess until the user is correct
+print("I have picked a random number from 1 to 100. Take a guess. \nI'll let you know if you are right and I'll guide you if you aren't.")
 
 while(random_guess != my_random):
+    random_guess = int(input("Guess my random number: "))
+    guess_count += 1
     if(random_guess > my_random):
         print("Your guess was higher than my number. Try again.")
-    else:
+    elif(random_guess < my_random):
         print("Your guess was lower than my number. Try again.")
-    guess_count += 1
-    break
-#while the input is not the correct number, it will check whether the guess was higher or lower and return a response
+    else:
+        break
+#through line 13 to line 18, while the input is not the correct number, it will check whether the guess was higher or lower and return a response
+#if the number guessed was correct, break out of the loop and conclude with a true if-true statement
 
 if(random_guess == my_random):
     print("Correct!")
     print("Your guess count was: " + str(guess_count))
-#informs when the user is correct and displays the total amount of guesses
-
-#    if(guess_count == 5):
-#        print("Your guess count was: " + str(guess_count))
-##intial loop breaker for v1
-
-
-#random guess checker
-#yay
+#informs when the user is correct and displays the total amount of guesses in that session
