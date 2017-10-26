9692b1340e01795f69cf5b8dd58424b2ff7a751a
SamOnline <gocsamo@gmail.com>
2017-10-26 10:15:39 +0100

    updated commit of task 2, yet to implement iterative guess functionality

--- ex11.py
+++ ex11.py
@@ -1,19 +1,32 @@
 import random
 from random import randint
 
-guess_count = 0
+guess_count = 1
+#initialises the guess count to 1 and increment for every wrong input attempt
 
 my_random = random.randint(1,5)
+#a random int is selected between 1 and 5, inclusive
 
 random_guess = int(input("Guess my random number: "))
+#a variable to require an input for each guess until the user is correct
 
 while(random_guess != my_random):
-    print("Sorry. Wrong! Try again.")
+    if(random_guess > my_random):
+        print("Your guess was higher than my number. Try again.")
+    else:
+        print("Your guess was lower than my number. Try again.")
     guess_count += 1
-    random_guess
-    if(guess_count == 3):
-        break
+    break
+#while the input is not the correct number, it will check whether the guess was higher or lower and return a response
 
+if(random_guess == my_random):
+    print("Correct!")
+    print("Your guess count was: " + str(guess_count))
+#informs when the user is correct and displays the total amount of guesses
+
+#    if(guess_count == 5):
+#        print("Your guess count was: " + str(guess_count))
+##intial loop breaker for v1
 
 
 #random guess checker
