46b928231489dbd45fd07b161364ca395d9f4d45
SamOnline <gocsamo@gmail.com>
2017-10-26 12:01:36 +0100

    initial commit of task 3 week 4 workseheet, drafting code

---
+++ ex12.py
@@ -0,0 +1,18 @@
+#night_cost = 70
+#
+#nights_stayed = int
+#
+#def hotel_cost(nights):
+#    nights = int(input("How many nights are you staying for?"))
+#    total_night_cost = nights * night_cost
+#    
+#
+#return hotel_cost(nights)
+#
+#
+#
+#
+#
+#
+#
+#
